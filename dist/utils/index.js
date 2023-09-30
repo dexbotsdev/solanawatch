cb6a6adec59342757ffdb1879e977e21bbcb44b1
dexbotdev <123926754+dexbotdev@users.noreply.github.com>
2023-09-30 20:12:30 +0530

    Delete dist directory
    w

--- dist/utils/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.shorten = void 0;
-const shorten = (str) => {
-    if (str.length < 10)
-        return str;
-    return `${str.slice(0, 6)}...${str.slice(str.length - 4)}`;
-};
-exports.shorten = shorten;
